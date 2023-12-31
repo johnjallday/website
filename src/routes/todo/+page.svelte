5826e12944913a57c0fb5171ac8bce71c0d32ec6
jj314 <yjyohahnjo@gmail.com>
2023-12-31 09:57:33 -0800

    Update +page.svelte

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -9,7 +9,8 @@
 <p>To-do json to html</p>
 <p>SVG Page Setup</p>
 <p>On-Going Projects</p>
-<p>Contact Form</p>
+<p>CSS/SVG for Contact</p>
 <p>discord integration</p>
 <p>easy-update-app</p>
 <p>duckdb backend</p>
+<p>github ssh-key setup</p>
