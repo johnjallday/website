4c775ae433b03099213f17f94797231db2473d67
jj314 <johnj.alldayok@gmail.com>
2024-01-13 20:33:06 -0800

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from 'mdsvex';
 import { preprocessMeltUI, sequence } from '@melt-ui/pp'
 
